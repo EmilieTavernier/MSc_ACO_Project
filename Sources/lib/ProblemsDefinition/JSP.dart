af6b992d5406c819b4dd3f2e9da64face39b302e
EmilieTavernier <47278505+EmilieTavernier@users.noreply.github.com>
2021-08-16 11:47:33 +0200

    Update JSP.dart

--- Sources/lib/ProblemsDefinition/JSP.dart
+++ Sources/lib/ProblemsDefinition/JSP.dart
@@ -1,17 +1,21 @@
 import 'dart:math';
 
-
+// This class holds the definition of JSP problem
 class JSP {
   var jobs = <Job>[];
-  var nbMaxTasks = 0;
-  var maxMachine = 0;
+  var nbMaxTasks = 0; // Max number of tasks composing a job
+  var maxMachine = 0; // Max machine number.
 
+  // Constructor accept user input parameters (list of jobs description)
+  // A job description a list of tasks defined as follow: (task1_machine, task1_duration);(t2_machine, t2_duration)...
+  // Example: (0,4);(2,5);(2;1)
   JSP(List<String> jobsDescription){
     if(jobsDescription.isEmpty) return;
-    print('jobsDescription: $jobsDescription');
+    //print('jobsDescription: $jobsDescription');
 
-    RegExp exp = new RegExp(r"\d+");
+    RegExp exp = new RegExp(r"\d+"); // Regex to match integers
 
+    // Define departure node (dummy job)
     Job start = new Job();
     start.tasks.add(new Task(-1, 0, Point(0, (jobsDescription.length+1)/2), 0));
     jobs.add(start);
@@ -22,9 +26,12 @@ class JSP {
       var tasksDescription = exp.allMatches(jobsDescription[i]).toList();
 
       for(int j=0; j<tasksDescription.length; j+=2){
+        // j+=2 -> integer at j = machine; integer a j+1 = duration
         int machine = int.parse(tasksDescription[j].group(0)!);
         int duration = int.parse(tasksDescription[j+1].group(0)!);
         int id = jobs.length * 100 + job.tasks.length;
+        // Example: Job 2 task 1 id is 201
+        // This allow a maximum of 99 tasks (limited to 9 in job description input field)
 
         job.tasks.add(new Task(machine, duration, Point(j/2+1, i+1), id));
         if(machine > maxMachine) maxMachine = machine;
@@ -56,20 +63,25 @@ class JSP {
   }
 }
 
+// Definition of a job...
 class Job {
+  // .. is just a list of tasks
   var tasks = <Task>[];
 }
 
+// Definition of a task
 class Task {
   late int machine;
   late int duration;
   late Point coordinates; // for visual representation
-  late var id;
+  late var id; 
+  // ID example: Job 2 task 1 ID is 201
+  // This allows a maximum of 99 tasks (limited to 9 in job description input field)
 
   List<Task> successors = [];
   List<double> pheromonesConcentration = []; // Pheromones on edge (Task-successor)
 
-
+  // Constructor 
   Task(machine, duration, coordinates, id){
     this.machine = machine;
     this.duration = duration;
