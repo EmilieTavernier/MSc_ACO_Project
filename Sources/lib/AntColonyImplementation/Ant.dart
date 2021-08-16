b6f6d27b4dbaddbfd6966a0f7c1028e22ebc092b
EmilieTavernier <47278505+EmilieTavernier@users.noreply.github.com>
2021-08-16 09:55:01 +0200

    Update Ant.dart

--- Sources/lib/AntColonyImplementation/Ant.dart
+++ Sources/lib/AntColonyImplementation/Ant.dart
@@ -2,11 +2,12 @@ import 'dart:collection';
 
 import '../ProblemsDefinition/JSP.dart';
 
+// This class defines an ant for all ACO implementations
 class Ant {
   var previousNode = 0;
   var currentNode = 0;
-  List<int> solution = [];
+  List<int> solution = []; // List of nodes id 
   List<List<Task>> schedule = []; // JSP Specific
-  HashSet<int> solutionHashSet = HashSet.of([]);
+  HashSet<int> solutionHashSet = HashSet.of([]); // for computation optimisation
   var solutionLength;
 }
