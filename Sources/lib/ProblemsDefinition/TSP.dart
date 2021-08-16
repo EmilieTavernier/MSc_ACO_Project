66a0ed865493a3446c1700b7a6e8b59e56fa4755
EmilieTavernier <47278505+EmilieTavernier@users.noreply.github.com>
2021-08-16 11:48:53 +0200

    Update TSP.dart

--- Sources/lib/ProblemsDefinition/TSP.dart
+++ Sources/lib/ProblemsDefinition/TSP.dart
@@ -1,11 +1,13 @@
 import 'dart:math';
 
+// This class defines the traveling salesman problem
 class TSP {
-  var cities = <Point>[];
+  var cities = <Point>[]; // List of coordinates
 
   TSP({int nbCities = 5}){
     final Random rng = new Random();
 
+    // The cities coordinates are defined randomly
     for(int i=0; i<nbCities; i++){
       cities.add( Point(rng.nextDouble(), rng.nextDouble()) );
     }
